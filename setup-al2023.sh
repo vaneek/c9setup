baa4b9fb66368befc58c5b458dc1a707b23d3afa
vaneek <vaneek@users.noreply.github.com>
2024-02-11 21:02:09 +0100

    Create setup-al2023.sh
    initial

---
+++ setup-al2023.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+sudo yum update
+sudo yum install gcc make patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+. ~/.bashrc
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+# ## snip .bashrc
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+# if command -v pyenv 1>/dev/null 2>&1; then
+#   eval "$(pyenv init -)"
+# fi
+# eval "$(pyenv virtualenv-init -)"
+# ##
+
+# . ~/.bashrc
+# pyenv virtualenv 3.9.16 cdk-project
+# pyenv local cdk-project
+# pip install git-remote-codecommit
