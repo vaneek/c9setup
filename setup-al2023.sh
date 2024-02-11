79fe70b20490cb018eb52bd020ae89aa58ee1376
vaneek <vaneek@users.noreply.github.com>
2024-02-11 21:28:02 +0100

    Update setup-al2023.sh

--- setup-al2023.sh
+++ setup-al2023.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo yum update
-sudo yum install gcc make patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
+sudo yum -y update
+sudo yum -y install gcc make patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 export PYENV_ROOT="$HOME/.pyenv"
